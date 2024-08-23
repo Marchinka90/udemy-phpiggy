e391e938eed141675e9c2f1641fa4357d730ecc3
Marchinka90 <martin_marchev@abv.bg>
2024-08-23 17:04:50 +0300

    Added Validation Service in the App. Added Validator, Rules and RuleInterface in Framework

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
+use App\Services\ValidatorService;
 
 class AuthController
 {
-  public function __construct(private TemplateEngine $view) {}
+  public function __construct(
+    private TemplateEngine $view,
+    private ValidatorService $validatorService
+  ) {}
+
   public function registerView()
   {
     echo  $this->view->render('register.php', [
@@ -18,6 +23,6 @@ class AuthController
 
   public function register()
   {
-    dd($_POST);
+    $this->validatorService->validateRegister($_POST);
   }
 }
