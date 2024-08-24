aa05e9ace7c15ce4d3933d6560ebc9c510d24bfa
Marchinka90 <martin_marchev@abv.bg>
2024-08-24 14:39:09 +0300

    Added new middleware for flash messages from session and added them to register form

--- src/App/views/register.php
+++ src/App/views/register.php
@@ -11,6 +11,11 @@
         name="email"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="john@example.com" />
+      <?php if (array_key_exists('email', $errors)) : ?>
+        <div class="bg-gray-100 mt-2 p-2 text-red-500">
+          <?= e($errors['email'][0]) ?>
+        </div>
+      <?php endif; ?>
     </label>
     <!-- Age -->
     <label class="block">
@@ -20,6 +25,12 @@
         name="age"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="" />
+
+      <?php if (array_key_exists('age', $errors)) : ?>
+        <div class="bg-gray-100 mt-2 p-2 text-red-500">
+          <?= e($errors['age'][0]) ?>
+        </div>
+      <?php endif; ?>
     </label>
     <!-- Country -->
     <label class="block">
@@ -32,6 +43,12 @@
         <option value="Mexico">Mexico</option>
         <option value="Invalid">Invalid Country</option>
       </select>
+
+      <?php if (array_key_exists('country', $errors)) : ?>
+        <div class="bg-gray-100 mt-2 p-2 text-red-500">
+          <?= e($errors['country'][0]) ?>
+        </div>
+      <?php endif; ?>
     </label>
     <!-- Social Media URL -->
     <label class="block">
@@ -41,6 +58,12 @@
         type="text"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="" />
+
+      <?php if (array_key_exists('socialMediaURL', $errors)) : ?>
+        <div class="bg-gray-100 mt-2 p-2 text-red-500">
+          <?= e($errors['socialMediaURL'][0]) ?>
+        </div>
+      <?php endif; ?>
     </label>
     <!-- Password -->
     <label class="block">
@@ -50,6 +73,12 @@
         type="password"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="" />
+
+      <?php if (array_key_exists('password', $errors)) : ?>
+        <div class="bg-gray-100 mt-2 p-2 text-red-500">
+          <?= e($errors['password'][0]) ?>
+        </div>
+      <?php endif; ?>
     </label>
     <!-- Confirm Password -->
     <label class="block">
@@ -60,6 +89,11 @@
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
         placeholder="" />
     </label>
+    <?php if (array_key_exists('confirmPassword', $errors)) : ?>
+      <div class="bg-gray-100 mt-2 p-2 text-red-500">
+        <?= e($errors['confirmPassword'][0]) ?>
+      </div>
+    <?php endif; ?>
     <!-- Terms of Service -->
     <div class="block">
       <div class="mt-2">
@@ -71,6 +105,12 @@
               type="checkbox" />
             <span class="ml-2">I accept the terms of service.</span>
           </label>
+
+          <?php if (array_key_exists('tos', $errors)) : ?>
+            <div class="bg-gray-100 mt-2 p-2 text-red-500">
+              <?= e($errors['tos'][0]) ?>
+            </div>
+          <?php endif; ?>
         </div>
       </div>
     </div>
